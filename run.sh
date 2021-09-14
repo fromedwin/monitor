2f2162f583d8f76ebbb32efab161a90e9d29cfd8
Sébastien Barbier <contact@sebastienbarbier.com>
2021-09-14 15:48:16 +0200

    Define default value for env variables

--- run.sh
+++ run.sh
@@ -6,6 +6,13 @@ if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
+# Set default nginx files as local
+if [[ -z "${NGINX}" ]]; then export NGINX="local"
+fi
+# Set default port to access dashboard
+if [[ -z "${PORT}" ]]; then export PORT=8000
+fi
+
 echo "Loading nginx/$NGINX files"
 
 docker-compose up -d
