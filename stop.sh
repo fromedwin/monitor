f0103b246fbb136ca4191e7d4af9dd3c915014bf
Sébastien Barbier <contact@sebastienbarbier.com>
2021-09-23 20:36:57 +0200

    Add stop.sh script to stop docker-compose with local env loaded

---
+++ stop.sh
@@ -0,0 +1,13 @@
+# If ./.env file exist, we export variables to current system to display later
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+# Set default nginx files as local
+if [[ -z "${NGINX}" ]]; then export NGINX="local"
+fi
+# Set default port to access dashboard
+if [[ -z "${PORT}" ]]; then export PORT=8000
+fi
+
+docker-compose down
