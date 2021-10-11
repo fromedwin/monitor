813c4a7b2396f10d9db719b613de91bf5d8960dd
Sébastien Barbier <contact@sebastienbarbier.com>
2021-10-11 20:55:41 +0200

    Include GeoIP2 within NGINX image

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# If ./.env file exist, we export variables to current system to display later
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+cd nginx
+
+mkdir -p 'geolite'
+
+wget "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" -O "./geolite/GeoLite2-City.tar.gz"
+
+wget "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=$MAXMIND_KEY&suffix=tar.gz" -O "./geolite/GeoLite2-Country.tar.gz"
+
+cd geolite
+
+tar -xf "./GeoLite2-City.tar.gz"
+tar -xf "./GeoLite2-Country.tar.gz"
+
+find . -iname "GeoLite2-City.mmdb" -exec mv {} . \;
+find . -iname "GeoLite2-Country.mmdb" -exec mv {} . \;
+
+# docker-compose pull
