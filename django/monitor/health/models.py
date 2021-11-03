054c45620b531bf0d4648136a132d0d7458f509a
Sébastien Barbier <contact@sebastienbarbier.com>
2021-11-03 15:50:54 +0100

    New UI for projects view

--- django/monitor/health/models.py
+++ django/monitor/health/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from applications.models import Service
 
 # Create your models here.
 class HealthTest(models.Model):
@@ -16,6 +17,14 @@ class HealthTest(models.Model):
         on_delete = models.CASCADE,
         related_name = "healthTest",
         related_query_name = "",
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete = models.CASCADE,
+        related_name = "healthTest",
+        related_query_name = "",
+        blank=True,
+        null=True,
     )
     code = models.IntegerField(choices=HTTP_CODES)
     comment = models.CharField(max_length=256, blank=True, help_text="Max 258 characters")
