522fa8e468b116a8a5f2061ae1e0f355aa18e0c7
Sébastien Barbier <contact@sebastienbarbier.com>
2021-11-24 17:43:42 +0000

    First draft for incident/degradated during last 30 days graph

--- django/monitor/projects/views.py
+++ django/monitor/projects/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.conf import settings
 from django.urls import reverse
-
-from rest_framework.authtoken.models import Token
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.shortcuts import render
-from clients.models import Server
-import datetime
 from django.utils import timezone
-
-from allauth.socialaccount.models import SocialApp
-from projects.models import Project, HTTPCodeService, HTTPMockedCodeService
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
-
 from django.contrib.auth.decorators import login_required
 
-from projects.forms import ProjectForm, ServiceForm, HTTPCodeServiceForm, MockedHTTPCodeServiceForm
+from rest_framework.authtoken.models import Token
+from allauth.socialaccount.models import SocialApp
+
+from projects.models import Project, HTTPCodeService, HTTPMockedCodeService
+from clients.models import Server
 from incidents.models import InstanceDownIncident
+from projects.forms import ProjectForm, ServiceForm, HTTPCodeServiceForm, MockedHTTPCodeServiceForm
 
 @login_required
 def projects(request):
@@ -33,12 +31,27 @@ def project(request, id):
 
     project = get_object_or_404(Project, pk=id)
 
-    start_date = timezone.now() - timezone.timedelta(days=30)
-    incidents = InstanceDownIncident.objects.filter(service__project=project, startsAt__gte=start_date)
+    number_days = 30
+
+    start_date = timezone.now() - timezone.timedelta(days=number_days)
+    incidents = InstanceDownIncident.objects.filter(service__project=project, startsAt__gte=start_date, severity=2)
+
+    days = []
+    for day in reversed(range(number_days)):
+        day = timezone.now() - timezone.timedelta(days=day)
+        start_of_day = timezone.datetime(day.year,day.month,day.day)
+        end_of_day = start_of_day + timezone.timedelta(days=1)
+
+        days.append({
+            'day': start_of_day,
+            'outrage': InstanceDownIncident.objects.filter(service__project=project, severity=2, service__is_critical=True).filter(Q(startsAt__gte=start_of_day, endsAt__lt=end_of_day)|Q(startsAt__lt=start_of_day, endsAt__gt=start_of_day)|Q(startsAt__lt=end_of_day, endsAt__gt=end_of_day)),
+            'degradated': InstanceDownIncident.objects.filter(service__project=project, severity=2, service__is_critical=False).filter(Q(startsAt__gte=start_of_day, endsAt__lt=end_of_day)|Q(startsAt__lt=start_of_day, endsAt__gt=start_of_day)|Q(startsAt__lt=end_of_day, endsAt__gt=end_of_day)),
+        })
 
     return render(request, 'projects/project_view.html', {
         'project': project,
         'incidents': incidents,
+        'days': days
     })
 
 @login_required
