66734ab6f09d7c895f4d994931e3d7d7311aee3d
Sébastien Barbier <contact@sebastienbarbier.com>
2021-12-07 18:19:15 +0000

    Remove input on collectstatic command within update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 docker exec -ti monitor_django python3 manage.py migrate
 docker exec -ti monitor_django python3 manage.py tailwind build
-docker exec -ti monitor_django python3 manage.py collectstatic
+docker exec -ti monitor_django python3 manage.py collectstatic --noinput
