e5b07a272d3a64c04fa5cd08503e52a8628c5a21
Sébastien Barbier <contact@sebastienbarbier.com>
2023-02-03 10:10:30 +0100

    Set LIGHTHOUSE_SCRAPE_INTERVAL_MINUTES as env var with default value at 60

--- django/fromedwin/settings.py
+++ django/fromedwin/settings.py
@@ -46,7 +46,7 @@ WEBAUTH_PASSWORD = os.environ.get('WEBAUTH_PASSWORD')
 IS_SERVICE_DOWN_SCRAPE_INTERVAL_MINUTE = 1
 IS_SERVICE_DOWN_TRIGGER_OUTRAGE_MINUTES = 5
 # Run Lighthouse every 60 minutes
-LIGHTHOUSE_SCRAPE_INTERVAL_MINUTES = 1
+LIGHTHOUSE_SCRAPE_INTERVAL_MINUTES = os.environ.get('LIGHTHOUSE_SCRAPE_INTERVAL_MINUTES', 60)
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
