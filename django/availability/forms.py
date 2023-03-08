a4fdb10a2f3d53e6c924f7c8404ca380e48dab64
Sébastien Barbier <contact@sebastienbarbier.com>
2023-03-08 20:38:46 +0100

    Use http form with sheme as select for Performance and httpcode forms

--- django/availability/forms.py
+++ django/availability/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, URLField, CharField
 from .models import Service, HTTPCodeService, HTTPMockedCodeService
 
 class ServiceForm(ModelForm):
@@ -7,9 +7,37 @@ class ServiceForm(ModelForm):
         fields = ['title', 'is_public', 'is_enabled','is_critical']
 
 class HTTPCodeServiceForm(ModelForm):
+
+    url = URLField(help_text="URL must start with http:// or https://")
+    scheme = CharField(max_length=5, help_text="URL must start with http:// or https://")
+
     class Meta:
         model = HTTPCodeService
-        fields = ['url']
+        fields = ['url', 'scheme']
+
+    # On init we set scheme value with http if url start with https or http if url start with url
+    def __init__(self, *args, **kwargs):
+        super(HTTPCodeServiceForm, self).__init__(*args, **kwargs)
+        if self.instance.url:
+            if self.instance.url.startswith('https://'):
+                self.initial['scheme'] = 'https'
+                self.initial['url'] = self.instance.url.replace('https://', '')
+            else:
+                self.initial['scheme'] = 'http'
+                self.initial['url'] = self.instance.url.replace('http://', '')
+
+     # Override save method to save the project title with the url value
+    def save(self, commit=True):
+
+        url = self.cleaned_data['url'].replace('https://', '').replace('http://', '')
+        if self.cleaned_data['scheme'] == 'https':
+            url = 'https://' + url
+        else:
+            url = 'http://' + url
+
+        obj = super(HTTPCodeServiceForm, self).save(commit=False)
+        obj.url = url
+        return obj
 
 class MockedHTTPCodeServiceForm(ModelForm):
     class Meta:
--- django/performances/forms.py
+++ django/performances/forms.py
@@ -1,7 +1,35 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, URLField, CharField
 from .models import Performance
 
 class PerformanceForm(ModelForm):
+
+    url = URLField(help_text="URL must start with http:// or https://")
+    scheme = CharField(max_length=5, help_text="URL must start with http:// or https://")
+
     class Meta:
         model = Performance
-        fields = ['url']
+        fields = ['scheme', 'url']
+
+    # On init we set scheme value with http if url start with https or http if url start with url
+    def __init__(self, *args, **kwargs):
+        super(PerformanceForm, self).__init__(*args, **kwargs)
+        if self.instance.url:
+            if self.instance.url.startswith('https://'):
+                self.initial['scheme'] = 'https'
+                self.initial['url'] = self.instance.url.replace('https://', '')
+            else:
+                self.initial['scheme'] = 'http'
+                self.initial['url'] = self.instance.url.replace('http://', '')
+
+     # Override save method to save the project title with the url value
+    def save(self, commit=True):
+
+        url = self.cleaned_data['url'].replace('https://', '').replace('http://', '')
+        if self.cleaned_data['scheme'] == 'https':
+            url = 'https://' + url
+        else:
+            url = 'http://' + url
+
+        obj = super(PerformanceForm, self).save(commit=False)
+        obj.url = url
+        return obj
