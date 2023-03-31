6166b5cd2df35bec39199d33d1d53e00486f2a3c
Sébastien Barbier <contact@sebastienbarbier.com>
2023-03-31 17:09:34 +0200

    Add form for notifications

--- django/notifications/admin.py
+++ django/notifications/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Pager_Duty
+from .models import Pager_Duty, Emails
 
 class PagerDutyAdmin(admin.ModelAdmin):
     list_display = ('project', 'routing_key')
 
+class EmailsAdmin(admin.ModelAdmin):
+    list_display = ('project', 'email')
+
 admin.site.register(Pager_Duty, PagerDutyAdmin)
+admin.site.register(Emails, EmailsAdmin)
--- django/notifications/urls.py
+++ django/notifications/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import pagerduty_form, pagerduty_delete, project_notifications
+from .views import pagerduty_form, pagerduty_delete, project_notifications, email_form, email_delete
 
 urlpatterns = [
     # Show notification panel for project id
@@ -8,4 +8,8 @@ urlpatterns = [
     path('project/<int:application_id>/notifications/pagerduty/add', pagerduty_form, name='pagerduty_add'),
     path('project/<int:application_id>/notifications/pagerduty/<int:pagerduty_id>/edit', pagerduty_form, name='pagerduty_edit'),
     path('project/<int:application_id>/notifications/pagerduty/<int:pagerduty_id>/delete', pagerduty_delete, name='pagerduty_delete'),
+    # manage pagerduty id for alerts and notifications
+    path('project/<int:application_id>/notifications/emails/add', email_form, name='email_add'),
+    path('project/<int:application_id>/notifications/emails/<int:email_id>/edit', email_form, name='email_edit'),
+    path('project/<int:application_id>/notifications/emails/<int:email_id>/delete', email_delete, name='email_delete'),
 ]
