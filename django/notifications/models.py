dcf222950ef2a2501983b4893f471a3090bbae4e
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-05 22:02:01 +0200

    Add migrations and fix for Notification model

--- django/notifications/models.py
+++ django/notifications/models.py
@@ -14,12 +14,14 @@ class Notification(models.Model):
         Project,
         on_delete = models.CASCADE,
         related_name = "notifications",
+        blank = True,
         null = True
     )
     service = models.ForeignKey(
         Service,
         on_delete = models.CASCADE,
         related_name = "notifications",
+        blank = True,
         null = True
     )
     severity = models.IntegerField(
