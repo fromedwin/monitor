489958ff84eb99c122c500be6b91002349d68f6b
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-05 21:38:07 +0200

    Fix bug with hasattr instead of blind call

--- django/incidents/handler.py
+++ django/incidents/handler.py
@@ -20,7 +20,7 @@ from constants import INCIDENT_STATUS, INCIDENT_SEVERITY
 
 def handleAlert(request, alert):
 
-    service = Service.objects.get(pk=alert["labels"]["service"]) if alert["labels"]["service"] else None
+    service = Service.objects.get(pk=alert["labels"]["service"]) if hasattr(alert["labels"], 'service') else None
 
     incident = Incident(
         alert_name = alert["labels"]["alertname"],
