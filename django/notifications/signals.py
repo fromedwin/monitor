a356ecccaac40a29ca312b85f01c5556237e9613
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-05 16:52:11 +0200

    Final version for notifications

--- django/notifications/signals.py
+++ django/notifications/signals.py
@@ -40,24 +40,16 @@ def create_notifications(sender, instance=None, **kwargs):
             ).save()
 
 
-@receiver(pre_save, sender=Incident)
+@receiver(pre_save, sender=Notification)
 def send_notifications(sender, instance=None, **kwargs):
 
     is_created = not bool(instance.pk)
-    is_modified = False
-
-    if not is_created:
-        old_instance = sender.objects.get(pk=instance.pk)
-        if old_instance.status != instance.status:
-            is_modified = True
-        if old_instance.severity != instance.severity:
-            is_modified = True
-
-    if is_created or is_modified:
+    
+    if is_created:
         # Test if instance has related model service
         try:
-            if instance.service_incidents:
-                emails = instance.service_incidents.service.project.emails.all()
+            if instance.service:
+                emails = instance.service.project.emails.all()
                 if len(emails) and emails[0]:
                     send_emails(instance, emails[0].email)
         except Exception as e:
--- django/notifications/utils.py
+++ django/notifications/utils.py
@@ -2,28 +2,28 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from constants import INCIDENT_STATUS, INCIDENT_SEVERITY
+from constants import NOTIFICATION_SEVERITY
 
-def send_emails(incident, email):
+def send_emails(notification, email):
 
-    status = 'Unknown'
+    subject = ''
 
-    if incident.severity == INCIDENT_SEVERITY['WARNING']:
-        status = 'Warning'
-    elif incident.severity == INCIDENT_SEVERITY['CRITICAL']:
-        status = 'Critical'
+    if notification.severity == NOTIFICATION_SEVERITY['WARNING']:
+        subject = "⚠️ " + notification.service.project.title+ " - " + notification.service.title
 
-    if incident.status == INCIDENT_STATUS['RESOLVED']:
-        status = 'Resolved'
+    if notification.severity == NOTIFICATION_SEVERITY['CRITICAL']:
+        subject = "❌ " + notification.service.project.title+ " - " + notification.service.title
 
-    summary = incident.summary
-    description = incident.description
-    subject = f'[{status}] {summary}'
-    html_message = render_to_string('notifications/emails/instance_down_email.html', {
-        'summary': summary, 
-        'description': description
-    })
-    plain_message = strip_tags(html_message)
+    if notification.severity == NOTIFICATION_SEVERITY['RESOLVED']:
+        subject = "✅ " + notification.service.project.title+ " - " + notification.service.title
+
+    if notification.severity == NOTIFICATION_SEVERITY['UNKNOWN']:
+        subject = "🤖 - " + notification.service.project.title+ " - " + notification.service.title
+
+    html_message = render_to_string('notifications/emails/notification_email.html', {
+        'notification': notification, 
+    }).strip()
+    plain_message = strip_tags(html_message).strip()
     from_email = f"{settings.CONTACT_NAME} <{settings.CONTACT_EMAIL}>"
     to = [email]
 
