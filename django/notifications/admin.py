608989c86eefedfbd91f21754040ae331df3ba71
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-05 12:07:39 +0200

    Improve admin to notification list

--- django/notifications/admin.py
+++ django/notifications/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Emails, Notification
+from django.utils.html import strip_tags
+from django.template.defaultfilters import truncatechars
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('date', 'severity')
+    list_display = ('date', 'message_stripped', 'user', 'severity')
+
+    @admin.display()
+    def message_stripped(self, obj):
+        return truncatechars(strip_tags(obj.message), 50)
+
+    @admin.display()
+    def user(self, obj):
+        return obj.service.project.user if obj.service else None
 
 class EmailsAdmin(admin.ModelAdmin):
     list_display = ('project', 'email')
