719348687ead30c49eb54f93dbc043409a3c1381
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-26 12:04:21 +0200

    Release v0.9.1

--- django/core/settings.py
+++ django/core/settings.py
@@ -31,7 +31,7 @@ if os.environ.get('SENTRY_DSN'):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-VERSION = [0, 9, 0]
+VERSION = [0, 9, 1]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
