743c637b6c22b3cb6c77876ac668ac374da7d49e
Sébastien Barbier <contact@sebastienbarbier.com>
2024-05-20 22:11:18 +0200

    Fix incident duration calculation
    Related to #29

--- django/incidents/utils.py
+++ django/incidents/utils.py
@@ -19,8 +19,14 @@ def getSeverity(alert):
         return INCIDENT_SEVERITY['CRITICAL']
     return INCIDENT_SEVERITY['UNKNOWN'] 
 
-def getStartsAt(alert):
+def getStartsAt(alert, settings):
     starts_at = timezone.make_aware(timezone.datetime.strptime(alert["startsAt"].split(".")[0], "%Y-%m-%dT%H:%M:%S"))
+    # Based on the severity, we update starting time to include the time before the alert
+    severity = getSeverity(alert)
+    if severity == INCIDENT_SEVERITY['CRITICAL']:
+        starts_at = starts_at - timedelta(minutes=settings.IS_SERVICE_DOWN_TRIGGER_OUTRAGE_MINUTES)
+    elif severity == INCIDENT_SEVERITY['WARNING']:
+        starts_at = starts_at - timedelta(minutes=settings.IS_SERVICE_DOWN_TRIGGER_WARNING_MINUTES)
     return starts_at
 
 def getEndsAt(alert):
