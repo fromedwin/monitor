3f005234095af4ea4f52337c1c341ffa4c7b816c
Sébastien Barbier <contact@sebastienbarbier.com>
2025-08-01 15:28:49 +0200

    Hide redirection because of tailslashes

--- src/fromedwin/views/pages.py
+++ src/fromedwin/views/pages.py
@@ -15,11 +15,20 @@ def project_pages(request, id):
 
     project = get_object_or_404(Project, pk=id)
 
+    # Filter out pages that are redirected to the same page but flagged because of tailslash redirect
+    pages_redirected = project.pages.filter(http_status=301).order_by('url')
+    pages_redirected_filtered = []
+    for page in pages_redirected:
+        url = page.url
+        redirect_url = page.outbound_links.first().to_page.url
+        if redirect_url != f'{url}/':
+            pages_redirected_filtered.append(page)
+
     return render(request, 'pages/pages.html', {
         'project': project,
         'pages_count': project.pages.count(),
         'pages': project.pages.filter(http_status__lt=300).exclude(http_status=0).order_by('url'),
-        'pages_redirected': project.pages.filter(http_status=301).order_by('url'),
+        'pages_redirected': pages_redirected_filtered,
         'pages_with_errors': project.pages.filter(
             Q(http_status__gte=400) | Q(http_status=0)
         ).order_by('url'),
