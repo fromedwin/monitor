6c448ce95533a619029dfba2b5eb1dd1bd510c5a
Sébastien Barbier <contact@sebastienbarbier.com>
2025-08-10 21:04:41 +0200

    Structure report

--- src/reports/tasks/create_report.py
+++ src/reports/tasks/create_report.py
@@ -12,11 +12,47 @@ def create_report(project_id, project_url):
     """
     start_time = time.time()
     
+
+    # Report should generate json contianing the following:
+    # - status: OK, WARNING, or ERROR
+    status = "OK"
+    # - Incidents: availablility
+    incidents = []
+    # - https when expiring
+    https_expiring = None
+    # - Lighthouse performance average for all pages.
+    #    - top 5 worst page to improve
+    lighthouse_performance = {
+        "average": 0,
+        "top_5_worst": [],
+    }
+    #    - Trend compare to previous report +2% - 5% ...
+    lighthouse_performance_trend = None
+    # - Counters: pages, 404, 301
+    counters = {
+        "pages": 0,
+        "404": 0,
+        "301": 0,
+    }
+
+
     # Create a simple report with status OK
     report_data = {
-        "status": "OK"
+        "status": status,
+        "incidents": [],
+        "https_expiring": None,
+        "lighthouse_performance": {
+            "average": 0,
+            "top_5_worst": [],
+        },
+        "lighthouse_performance_trend": None,   
+        "counters": {
+            "pages": 0,
+            "404": 0,
+            "301": 0,
+        },
     }
-    
+
     try:
         response = requests.post(f'{settings.BACKEND_URL}/api/save_project_report/{settings.SECRET_KEY}/{project_id}/', json={
             'report_data': report_data,
