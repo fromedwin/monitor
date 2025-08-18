a5e41ad4571ef15a87a470b4bbda59b2f8fa6056
Sébastien Barbier <contact@sebastienbarbier.com>
2025-08-18 07:58:05 +0200

    Add page lighthouse and previous report in report

--- src/reports/tasks/create_report.py
+++ src/reports/tasks/create_report.py
@@ -1,10 +1,12 @@
 from datetime import timedelta
 import time
 from celery import shared_task
+from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from projects.models import Project
 from reports.models import ProjectReport
 from logs.models import CeleryTaskLog
+from hashlib import sha256
 
 @shared_task()
 def create_report(project_id, project_url):
@@ -16,16 +18,29 @@ def create_report(project_id, project_url):
     # Get project to update
     project = get_object_or_404(Project, id=project_id)
     
+    # Get the previous report for this project, if any
+    previous_report = ProjectReport.objects.filter(project=project).order_by('-created_at').first()
 
     # Create a simple report with status OK
     report_data = {
+        "id": sha256(str(project.id).encode('utf-8')).hexdigest(),
+        "project_id": project.id,
+        "date": timezone.now().isoformat(),
         "pages": [
             {
                 "url": project_url,
                 "http_status": page.http_status,
+                "title": page.title,
+                "description": page.description,
+                "lighthouse_report": page.lighthouse_report.first().report_json_file.url if page.lighthouse_report.first() else None,
+                "lighthouse_report_date": page.lighthouse_report.first().creation_date if page.lighthouse_report.first() else None,
+                "lighthouse_report_form_factor": page.lighthouse_report.first().form_factor if page.lighthouse_report.first() else None,
+                "lighthouse_report_score_performance": page.lighthouse_report.first().score_performance if page.lighthouse_report.first() else None,
+                "lighthouse_report_score_accessibility": page.lighthouse_report.first().score_accessibility if page.lighthouse_report.first() else None,
             }
             for page in project.pages.all()
-        ]
+        ],
+        "previous_report": previous_report.data if previous_report else None,
     }
 
 
